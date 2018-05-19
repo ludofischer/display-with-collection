caa24ae9f1745aeaba581b4194c40da4ab433016
Ludovico Fischer <livrerie@gmail.com>
2018-05-19 22:23:36 +0200

    Rename notification to item in types.

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,10 @@
 import { ReactNode } from 'react';
 
-export type State<T> = { notifications: Map<number, T>, currentIndex: number };
+export type State<T> = { items: Map<number, T>, currentIndex: number };
 export type RenderArgs<T> = {
-  notifications: Array<T>;
-  addNotification: (notification: T) => void;
-  removeNotification: (index: number) => void;
+  items: Array<T>;
+  addItem: (item: T) => void;
+  removeItem: (index: number) => void;
 };
 export type RenderType<T> = (args: RenderArgs<T>) => ReactNode;
 export type Props<T> = { render: RenderType<T> };
