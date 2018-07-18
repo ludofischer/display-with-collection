2938c78a5209d756aed870b9da32a7c05d91cd8d
Ludovico Fischer <livrerie@gmail.com>
2018-07-18 16:41:14 +0200

    chore(package): import test utilities from jest-dom package.

--- src/__tests__/Notification.test.tsx
+++ src/__tests__/Notification.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent, cleanup } from 'react-testing-library';
-import 'dom-testing-library/extend-expect';
+import 'jest-dom/extend-expect';
 import ListOfSomething from '../index.tsx';
 
 describe('Displaying items', () => {
